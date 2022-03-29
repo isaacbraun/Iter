852b664289b2f95a795124867b1a784a33c4ebdf
Isaac Braun <isaacbraun2000@gmail.com>
2022-03-29 01:13:59 -0400

    Added Search Files

---
+++ iter-app/components/__tests__/Search.test.js
@@ -0,0 +1,16 @@
+import * as search from '../Search';
+
+describe("Search Return Cases", () => {
+    // test("ICAO ID", () => {
+    //     const output = {"month" : "Jan", "day" : 23, "hour" : 13};
+    //     expect(tools.dateFormatter(new Date(2022, 0, 23), 13)).toEqual(output);
+    // });
+    // test("Hours = 24", () => {
+    //     const output = {"month" : "Mar", "day" : 2, "hour" : 0};
+    //     expect(tools.dateFormatter(new Date(2022, 2, 1), 24)).toEqual(output);
+    // });
+    // test("Hours > 24", () => {
+    //     const output = {"month" : "Dec", "day" : 6, "hour" : 8};
+    //     expect(tools.dateFormatter(new Date(2022, 11, 5), 32)).toEqual(output);
+    // });
+});
