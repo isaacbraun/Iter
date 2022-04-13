65170439f1d0b885f980e461089bef5970abe459
Isaac Braun <isaacbraun2000@gmail.com>
2022-04-12 23:20:23 -0400

    Fixed/Added Tests

--- iter-app/components/__tests__/Search.test.js
+++ iter-app/components/__tests__/Search.test.js
@@ -1,70 +1,78 @@
 /* eslint-disable no-undef */
-import { Search, matches } from '../Search';
+import { matches } from '../Search';
 
 const airport = {
-    station_Code: ["KCLE"],
+    station_id: ["KCLE"],
     iata: "CLE",
     name: "Cleveland Hopkins International Airport",
     municipality: 'Cleveland'
 };
 
-// test("Single Character", () => {
-//     expect(search.matches(airport, "a").toEqual());
-// });
-
 describe("Matches Function", () => {
     test("Partial Lowercase ICAO Code", () => {
-        expect(matches(airport, "kc").toBeTruthy());
+        expect(matches(airport, "kc")).toBeTruthy();
     });
     test("Partial Uppercase ICAO Code", () => {
-        expect(matches(airport, "LE").toBeTruthy());
+        expect(matches(airport, "LE")).toBeTruthy();
     });
     test("Complete Lowercase ICAO Code", () => {
-        expect(matches(airport, "kcle").toBeTruthy());
+        expect(matches(airport, "kcle")).toBeTruthy();
     });
     test("Complete Uppercase ICAO Code", () => {
-        expect(matches(airport, "KCLE").toBeTruthy());
+        expect(matches(airport, "KCLE")).toBeTruthy();
+    });
+    test("Incorrect ICAO Code", () => {
+        expect(matches(airport, "KLAX")).toBeFalsy();
     });
     test("Partial Lowercase IATA Code", () => {
-        expect(matches(airport, "cl").toBeTruthy());
+        expect(matches(airport, "cl")).toBeTruthy();
     });
     test("Partial Uppercase IATA Code", () => {
-        expect(matches(airport, "CL").toBeTruthy());
+        expect(matches(airport, "CL")).toBeTruthy();
     });
     test("Complete Lowercase IATA Code", () => {
-        expect(matches(airport, "cle").toBeTruthy());
+        expect(matches(airport, "cle")).toBeTruthy();
     });
     test("Complete Uppercase IATA Code", () => {
-        expect(matches(airport, "CLE").toBeTruthy());
+        expect(matches(airport, "CLE")).toBeTruthy();
+    });
+    test("Incorrect IATA Code", () => {
+        expect(matches(airport, "LAX")).toBeFalsy();
     });
     test("Partial Lowercase Name 1", () => {
-        expect(matches(airport, "Cleveland").toBeTruthy());
+        expect(matches(airport, "Cleveland")).toBeTruthy();
     });
     test("Partial Uppercase Name 1", () => {
-        expect(matches(airport, "CLEVELAND").toBeTruthy());
+        expect(matches(airport, "CLEVELAND")).toBeTruthy();
     });
     test("Partial Lowercase Name 2", () => {
-        expect(matches(airport, "hopkins").toBeTruthy());
+        expect(matches(airport, "hopkins")).toBeTruthy();
     });
     test("Partial Uppercase Name 2", () => {
-        expect(matches(airport, "HOPKINS").toBeTruthy());
+        expect(matches(airport, "HOPKINS")).toBeTruthy();
     });
     test("Complete Lowercase Name", () => {
-        expect(matches(airport, "Cleveland Hopkins International Airport").toBeTruthy());
+        expect(matches(airport, "Cleveland Hopkins International Airport")).toBeTruthy();
     });
     test("Complete Uppercase Name", () => {
-        expect(matches(airport, "CLEVELAND HOPKINS INTERNATIONAL AIRPORT").toBeTruthy());
+        expect(matches(airport, "CLEVELAND HOPKINS INTERNATIONAL AIRPORT")).toBeTruthy();
+    });
+    test("Incorrect Name", () => {
+        expect(matches(airport, "Los angeles")).toBeFalsy();
     });
     test("Partial Lowercase Municipality", () => {
-        expect(matches(airport, "land").toBeTruthy());
+        expect(matches(airport, "land")).toBeTruthy();
     });
     test("Partial Uppercase Municipality", () => {
-        expect(matches(airport, "LAND").toBeTruthy());
+        expect(matches(airport, "LAND")).toBeTruthy();
     });
     test("Complete Lowercase Municipality", () => {
-        expect(matches(airport, "cleveland").toBeTruthy());
+        expect(matches(airport, "cleveland")).toBeTruthy();
     });
     test("Complete Uppercase Municipality", () => {
-        expect(matches(airport, "CLEVELAND").toBeTruthy());
+        expect(matches(airport, "CLEVELAND")).toBeTruthy();
+    });
+    test("Incorrect Municipality", () => {
+        expect(matches(airport, "Chattanooga")).toBeFalsy();
     });
 });
