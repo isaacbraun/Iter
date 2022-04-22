13e97c98794233146644c1a11ca53ccdab6d896e
isaacbraun <31631609+isaacbraun@users.noreply.github.com>
2022-04-22 01:54:51 -0400

    Initial Grading Functions - No Station Grade

--- iter-app/tools/Compare.js
+++ iter-app/tools/Compare.js
@@ -1,5 +1,9 @@
 // import React from 'react';
 import { Alert } from 'react-native';
+import { isLastDayOfMonth } from './Tools';
+
+// eslint-disable-next-line no-undef
+const converter = require('react-native-xml2js');
 
 // Check if Two Paths Have Same Origin/Destination
 // Also Does Input Validation and Alerts Reason if alert==true
@@ -91,74 +95,184 @@ export function speed(speed, altitude) {
     return speed * 1.15078;
 }
 
-// Return Estimated Travel Time in hours
-export function time(distance, speed) {
-    return distance / speed;
+// Return Estimated Travel Time in Hours
+export function time(distance, info) {
+    return distance / speed(info.speed, info.alti);
 }
 
-// https://stackoverflow.com/questions/46590154/calculate-bearing-between-2-points-with-javascript
-export function bearing(startLat, startLng, destLat, destLng){
-    startLat = Math.toRadians(startLat);
-    startLng = Math.toRadians(startLng);
-    destLat = Math.toRadians(destLat);
-    destLng = Math.toRadians(destLng);
-  
-    const y = Math.sin(destLng - startLng) * Math.cos(destLat);
-    const x = Math.cos(startLat) * Math.sin(destLat) -
-          Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
-    const brng = Math.toDegrees(Math.atan2(y, x));
-    return (brng + 360) % 360;
-}
+// Get Stations Along Flight Path Within 15 Mile Radius
+export function fetchStations(stationString) {
+    const url = `https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&flightPath=15;${stationString}&hoursBeforeNow=3&mostRecentForEachStation=constraint`;
 
-export function calculatePointsAlongPath(startLat, startLng, destLat, destLng) {
-    const radius = 3956;
-    const bearing = bearing(startLat, startLng, destLat, destLng);
-    const distance = distance(startLat, startLng, destLat, destLng);
-    const interval = 20;
-    let points = [];
-
-    for (let i = 1; i <= distance % interval; i++) {
-        const destDistance = interval * i;
-        const lat = Math.asin(Math.sin(startLat) * Math.cos(destDistance / radius) +
-                    Math.cos(startLat) * Math.sin(destDistance / radius) * Math.cos(bearing));
-        const lng = startLng + Math.atan2(Math.sin(bearing) * Math.sin(destDistance / radius) * Math.cos(startLat),
-                    Math.cos(destDistance / radius) - Math.sin(startLat) * Math.sin(lat));
-
-        
-        points.push([lat, lng]);
-    }
-    
-    return points;
-}
-
-export function getPoints(pointString) {
-    const converter = require('react-native-xml2js');
-
-    return fetch("https://www.aviationweather.gov/adds/dataserver_current/current/metars.cache.xml")
-    .then((response) => { return response.text() }).then(value => converter.parseString(value, function (err, result) {
-				setMetars(result.response.data[0].METAR);
-			}));
+    return fetch(url)
+    .then((response) => response.text())
     .catch((error) => {
         console.error(error);
-        return null;
     })
-    // https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&flightPath=15;KCLE;KCHA&hoursBeforeNow=3&mostRecentForEachStation=constraint
 }
 
-export function getPoints(path) {
-    let points = [];
-    for (let i = 1; i < path.length - 2; i++) {
-        points.push(path[i])
-        points.concat(calculatePointsAlongPath(
-            path[i].lattitude[0],
-            path[i].longitude[0],
-            path[i + 1].latitude[0],
-            path[i + 1].longitude[0]
-        ))
+// Takes Path and Returns Stations Along The Path
+// Includes Relevant Tafs to Estimated Time They Would Be Passed
+export async function getPathStations(path, info) {
+    let stations = [];
+    let stationString = "";
+
+    for (const point of path) {
+        stationString += `${point.station_id[0]};`;
     }
+
+    await fetchStations(stationString).then(value => converter.parseString(value, function (err, result) {
+        stations = result.response.data[0].METAR;
+    }))
+
+    stations = addTafs(stations, info, path[0].station_id[0])
+    
+    return stations;
+}
+
+// Get Tafs for Station With Valid Time Inside Start and End Time Parrams
+export function fetchTafs(id, start, end) {
+    const url = `https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&startTime=${start}&endTime=${end}&timeType=valid&stationString=${id}`;
+
+    return fetch(url)
+    .then((response) => response.text())
+    .catch((error) => {
+        console.error(error);
+    })
+}
+
+// Takes Array of Stations and Adds Tafs For Relevant Time
+export async function addTafs(stations_in, info, origin) {
+    let stations_out = stations_in;
+    let startYear = info.date.getYear();
+    let startMonth = info.date.getMonth();
+    let startDay = info.date.getDate();
+    let startHours = info.date.getHours();
+
+    for (let i = 0; i < stations_out.length; i++) {
+        if (stations_out[i].station_id[0] != origin) {
+            const travelTime = time(
+                distance(
+                    stations_out[i-1].latitude[0],
+                    stations_out[i-1].longitude[0],
+                    stations_out[i].latitude[0],
+                    stations_out[i].longitude[0],
+                ),
+                info
+            );
+            let destYear = startYear;
+            let destMonth = startMonth;
+            let destDay = startDay;
+            let destHours = startHours + travelTime;
+            if (destHours >= 24) {
+                if (destMonth == 11) {
+                    destYear += 1;
+                    destMonth = 0;
+                }
+                else if (isLastDayOfMonth(info.date)) {
+                    destMonth += 1;
+                    destDay = 1;
+                }
+                else {
+                    destDay = destDay + 1;
+                }
+                
+                destHours = destHours - 24;
+            }
+        
+            await fetchTafs(
+                stations_out[i].station_id[0],
+                new Date(destYear, destMonth, destDay, destHours - 1, 30),
+                new Date(destYear, destMonth, destDay, destHours, 30)
+            ).then(value => converter.parseString(value, function (err, result) {
+                stations_out[i].tafs = result.response.data[0].TAF;
+            }));
+        }
+    }
+
+    return stations_out;
+}
+
+// Takes Station and Returns Grade for Weather Condition
+export function gradeStation(station) {
+    return 100;
+}
+
+// Take Flight Path and Returns Grade for Weather Condition
+export function gradePath(path_in) {
+    // const info = path_in[0];
+    // const path = path_in.slice(1);
+    let path = path_in;
+    const info = path.shift();
+    
+    let grade = 0;
+    let amount = 0;
+
+    const stations = getPathStations(path, info);
+
+    for (const station of stations) {
+        grade += gradeStation(station);
+        amount ++;
+    }
+
+    return grade / amount;
 }
 
 export function compare(main, alt) {
-    Alert.alert("Compare Success");
-    
+    const mainGrade = gradePath(main);
+    const altGrade = gradePath(alt);
+
+    const result = mainGrade > altGrade;
+
+    Alert.alert(`The ${result ? "Main" : "Alternate"} Path has more favorable weather.`);
+    return `The ${result ? "Main" : "Alternate"} Path has more favorable weather.`;
 }
+
+// https://stackoverflow.com/questions/46590154/calculate-bearing-between-2-points-with-javascript
+// export function bearing(startLat, startLng, destLat, destLng){
+//     startLat = Math.toRadians(startLat);
+//     startLng = Math.toRadians(startLng);
+//     destLat = Math.toRadians(destLat);
+//     destLng = Math.toRadians(destLng);
+  
+//     const y = Math.sin(destLng - startLng) * Math.cos(destLat);
+//     const x = Math.cos(startLat) * Math.sin(destLat) -
+//           Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
+//     const brng = Math.toDegrees(Math.atan2(y, x));
+//     return (brng + 360) % 360;
+// }
+
+// export function calculatePointsAlongPath(startLat, startLng, destLat, destLng) {
+//     const radius = 3956;
+//     const bearing = bearing(startLat, startLng, destLat, destLng);
+//     const distance = distance(startLat, startLng, destLat, destLng);
+//     const interval = 20;
+//     let points = [];
+
+//     for (let i = 1; i <= distance % interval; i++) {
+//         const destDistance = interval * i;
+//         const lat = Math.asin(Math.sin(startLat) * Math.cos(destDistance / radius) +
+//                     Math.cos(startLat) * Math.sin(destDistance / radius) * Math.cos(bearing));
+//         const lng = startLng + Math.atan2(Math.sin(bearing) * Math.sin(destDistance / radius) * Math.cos(startLat),
+//                     Math.cos(destDistance / radius) - Math.sin(startLat) * Math.sin(lat));
+
+        
+//         points.push([lat, lng]);
+//     }
+    
+//     return points;
+// }
+
+// Manually Find Points Along Flight Path 
+// export function getPoints(path) {
+//     let points = [];
+//     for (let i = 1; i < path.length - 2; i++) {
+//         points.push(path[i])
+//         points.concat(calculatePointsAlongPath(
+//             path[i].lattitude[0],
+//             path[i].longitude[0],
+//             path[i + 1].latitude[0],
+//             path[i + 1].longitude[0]
+//         ))
+//     }
+// }
