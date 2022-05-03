8d390ac89ad04bcbcec9353883d9d1291a9ff91c
isaacbraun <31631609+isaacbraun@users.noreply.github.com>
2022-05-03 00:00:14 -0400

    Added LastDay Tests and Fixed dateFormatter

--- iter-app/tools/__tests__/Tools.test.js
+++ iter-app/tools/__tests__/Tools.test.js
@@ -1,6 +1,24 @@
 /* eslint-disable no-undef */
 import * as tools from '../Tools';
 
+describe("Is Last Day of Month", () => {
+    test("May 2nd, 22", () => {
+        expect(tools.isLastDayOfMonth(new Date(2022, 4, 2))).toBeFalsy();
+    });
+    test("Last Day of May 22", () => {
+        expect(tools.isLastDayOfMonth(new Date(2022, 4, 31))).toBeTruthy();
+    });
+    test("June 1st, 22", () => {
+        expect(tools.isLastDayOfMonth(new Date(2022, 5, 1))).toBeFalsy();
+    });
+    test("Last Day of January 22", () => {
+        expect(tools.isLastDayOfMonth(new Date(2022, 0, 31))).toBeTruthy();
+    });
+    test("Last Day of 2021", () => {
+        expect(tools.isLastDayOfMonth(new Date(2021, 11, 31))).toBeTruthy();
+    });
+});
+
 describe("Date Formatter", () => {
     test("Hours < 24", () => {
         const output = {"month" : "Jan", "day" : 23, "hour" : 13};
@@ -15,8 +33,12 @@ describe("Date Formatter", () => {
         expect(tools.dateFormatter(new Date(2022, 11, 5), 32)).toEqual(output);
     });
     test("Last Day of Month with Hour > 24", () => {
-        const output = {"month" : "May", "day" : 1, "hour" : 8};
-        expect(tools.dateFormatter(new Date(2022, 4, 30), 32)).toEqual(output);
+        const output = {"month" : "Jun", "day" : 1, "hour" : 8};
+        expect(tools.dateFormatter(new Date(2022, 4, 31), 32)).toEqual(output);
+    });
+    test("Last Day of 2021 > 24", () => {
+        const output = {"month" : "Jan", "day" : 1, "hour" : 8};
+        expect(tools.dateFormatter(new Date(2021, 11, 31), 32)).toEqual(output);
     });
 });
 
